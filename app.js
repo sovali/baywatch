e60578aec01b1c08033c7e9eccfd30cfd08f9fe5
Selin Ovali <sovali@purdue.edu>
2017-06-28 21:26:23 -0400

    Added a function to the delete button

--- app.js
+++ app.js
@@ -13,15 +13,41 @@ const app = {
 
   },
 
+  handleFav(ev) {
+    const currbutton = ev.target
+    const currflick = currbutton.parentElement
+    currflick.style.color = yellow;
+  },
+
+  handleDel(ev) {
+    const currbutton = ev.target
+    const currflick = currbutton.parentElement
+    
+    const i = this.flicks.indexOf(currflick)
+    this.flicks.splice(i,1)
+
+    currflick.parentNode.removeChild(currflick)
+  },
+
   renderListItem(flick) {
     const item = document.createElement('li')
     item.textContent = flick.name
+
     const favbutton = document.createElement('button')
     favbutton.textContent = "add to favorites"
     favbutton.setAttribute("class","favbutton")
+    favbutton.setAttribute("id",`f${this.id}`)
+    favbutton.addEventListener (
+        'click', this.handleFav.bind(this)
+    )
+
     const delbutton = document.createElement('button')
     delbutton.textContent = "delete"
     delbutton.setAttribute("class","delbutton")
+    delbutton.setAttribute("id", `d${this.id}`)
+    delbutton.addEventListener (
+        'click', this.handleDel.bind(this)
+    )
     item.appendChild(favbutton)
     item.appendChild(delbutton)
     return item
@@ -39,8 +65,7 @@ const app = {
 
     const listItem = this.renderListItem(flick)
     this.list.appendChild(listItem)
-    this.flicks[flick.id - 1] = flick.name
-    console.log(this.flicks)
+    this.flicks[flick.id - 1] = flick
 
     this.max ++
   },
